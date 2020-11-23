198392e0f343cabd38db87d76d4dd42d4f2d9fdf
Naaaaa0 <72750041+Naaaaa0@users.noreply.github.com>
2020-11-23 11:16:46 +0900

    section3-q1 change

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,29 +4,26 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("We like keyakizaka46");
+  balloon("We like keyakizaka46", 0,0,70,66,135,245,245,152,66);
 }
 
-function balloon(t){
+function balloon(t,x,y,z,a,b,c,d,e,f){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  let p = z;
   noStroke();
-  fill(185,245,66);
-
-  beginShape();
-  vertex(0,0);
-  vertex(w+p*2,0);
-  vertex(w+p*2,(h+p*2)*1/4);
-  vertex(8+w+p*2,(h+p*2)*1/2);
-  vertex(w+p*2,(h+p*2)*3/4);
-  vertex(w+p*2,h+p*2);
-  vertex(0,h+p*2);
-  endShape();
-
-  //rect(0, 0, w + p * 2, h + p * 2);
-  //triangle(w+p*2,(h+p*2)/4,w+p*2,(h+p*2)3/4, w+p*2+5,(h+p*2)/2)
-  fill(66, 72, 245);
-  text(t, p, (h + p)-6);
-
+  fill(a,b,c);
+  rect(x,y, w + z * 2, h + z * 2);
+  triangle(w+z*2,(h+z*2)/3,w+z*2+(w+z*2-x)/4,(h+z*2)/2,w+z*2,(h+z*2)*2/3)
+  fill(d,e,f);
+  text(t, z, h/2+z);
 }
+//beginShape();
+//vertex(0,0);
+//vertex(w+p*2,0);
+//vertex(w+p*2,(h+p*2)*1/4);
+//vertex(8+w+p*2,(h+p*2)*1/2);
+//vertex(w+p*2,(h+p*2)*3/4);
+//vertex(w+p*2,h+p*2);
+//vertex(0,h+p*2);
+//endShape();

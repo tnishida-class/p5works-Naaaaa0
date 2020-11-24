416a217e79f010e44e2330248d39986f92c1891d
Naaaaa0 <72750041+Naaaaa0@users.noreply.github.com>
2020-11-24 11:05:59 +0900

    finalwork apdate

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -43,37 +43,24 @@ function draw(){
     x=constrain(x,0,width);
     y=constrain(y,0,height);
   }
-  if(dist(mouseX,mouseY,a,b)&&dist(x,y,a,b)<20){
+  if(dist(mouseX,mouseY,a,b)&&dist(x,y,a,b)<10){
     vx=0;
     vy=0;
     wa=0;
     wb=0;
-    noFill();
-    for(let i = 0; i < 10; i++){
-     let r = (i+1)*10;
-     //ellipse(50,50,(i+1)*5)
-      if(i < 5){
-       stroke(255,0,0);
-      }
-      else{
-       stroke(0,0,255);
-      }
-      ellipse(50,50,r);
-    }
   }
 }
 
-function star(x, y, r, angle){
-  noStroke();
-  fill(255,204,0);
-  beginShape();
-  count= count+1;
-  for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI + count*angle;
-    var x = x + cos(theta) * r;
-    var y = y + sin(theta) * r;
-    vertex(x,y);
-  }
+function star(cx, cy, r){
+ noStroke();
+ fill(255, 204, 0);
+ beginShape();
+ for(let i = 0; i < 5; i++){
+  const theta = TWO_PI * i * 2 / 5 - HALF_PI;
+  const x = cx + cos(theta) * r;
+  const y = cy + sin(theta) * r;
+  vertex(x, y);
+ }
   endShape(CLOSE);
 }
 
